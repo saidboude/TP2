20e11783ce29350cf135a5d211fe712ad7eb69f6
saidboude <e9875172@cmaisonneuve.qc.ca>
2022-08-25 00:39:26 -0400

    changement de footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="site__footer">
 <h6>3800, rue Sherbrooke Est Montréal (Québec) H1X 2A2 | 514 254-7131
-    Copyright © 2014 - Collège de Maisonneuve. Tous droits réservés.</h6>
+    Copyright © 2022 - Collège de Maisonneuve. Tous droits réservés.</h6>
 </footer>
 <?php wp_footer(); ?>
 </section> <!-- fin .site -->
